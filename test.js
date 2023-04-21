ad0678c4e9eefd82519564b7c7123bbcf53c7859
Rhoda Pickens <rhodajane@gmail.com>
2023-04-21 10:31:51 -0500

    Bound methods to functions

--- test.js
+++ test.js
@@ -1,1 +1,11 @@
-// Your code here
+const {Employee} = require('./employee.js');
+
+let john = new Employee("John Wick", "Dog Lover");
+
+let sayNameFunc = john.sayName;
+let boundJohn = sayNameFunc.bind(john);
+setTimeout(boundJohn, 2000);
+
+let sayOccFunc = john.sayOccupation;
+let boundOcc = sayOccFunc.bind(john);
+setTimeout(boundOcc, 3000);
